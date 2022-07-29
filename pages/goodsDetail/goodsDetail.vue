4c65e23539f3cdecb04c9910459694132b12cced
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2022-07-29 16:41:22 +0800

    Create goodsDetail.vue

---
+++ pages/goodsDetail/goodsDetail.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad(query) {
+			console.log(query);
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
